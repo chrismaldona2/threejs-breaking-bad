9fe7ce48d76d18bcc7766f370720480d62f4b4b4
Christian Maldonado <chris.ariel.maldonado@gmail.com>
2025-04-24 23:26:31 -0300

    debug class refactor

--- src/experience/utils/Debug.ts
+++ src/experience/utils/Debug.ts
@@ -1,43 +1,26 @@
 import GUI from "lil-gui";
 
 class Debug {
-  static instance: Debug;
-  gui!: GUI;
-  private keydownHandler!: (event: KeyboardEvent) => void;
+  gui: GUI;
+  private keydownHandler: (event: KeyboardEvent) => void;
 
   constructor() {
-    if (Debug.instance) {
-      return Debug.instance;
-    }
-    Debug.instance = this;
     this.gui = new GUI({ title: "Tweaks" });
     this.gui.hide();
-    this.handleToggle();
-  }
 
-  show() {
-    this.gui.show();
-  }
-
-  handleToggle() {
     this.keydownHandler = (event: KeyboardEvent) => {
-      if (event.key.toLowerCase() === "h") {
-        this.gui.show(this.gui._hidden);
-      }
+      if (event.key.toLowerCase() === "h") this.toggle();
     };
     window.addEventListener("keydown", this.keydownHandler);
+  }
+
+  toggle() {
+    this.gui.show(this.gui._hidden);
   }
 
   dispose() {
     window.removeEventListener("keydown", this.keydownHandler);
     this.gui.destroy();
-  }
-
-  static getInstance(): Debug {
-    if (!Debug.instance) {
-      throw new Error("Debug not initialized yet");
-    }
-    return Debug.instance;
   }
 }
 
